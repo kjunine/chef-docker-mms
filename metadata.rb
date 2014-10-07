9cf7da6210d970ebf6a423f2db85b076f7dbaa93
Daniel Ku <kjunine@gmail.com>
2014-10-07 16:15:27 +0900

    Adds initial version.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'docker-mms'
+maintainer       'Daniel Ku'
+maintainer_email 'kjunine@gmail.com'
+license          'MIT'
+description      'Installs/Configures docker-mms'
+long_description 'Installs/Configures docker-mms'
+version          '0.1.0'
+
